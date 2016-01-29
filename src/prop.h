49a100c28275a71ec2e8377a58cbbcb81d4ff882
sigsegv <sigsegv@sigpipe.info>
2016-01-29 01:30:32 -0800

    fix prop accessors not having zero size by adjusting this ptr accordingly

--- src/prop.h
+++ src/prop.h
@@ -191,7 +191,7 @@ private:
 // dtor:     NOPE
 // virtual:  NOPE
 // members:  NOPE
-template<typename T, typename IPROP, IPROP *PROP>
+template<typename T, typename IPROP, IPROP *PROP, size_t *ADJUST>
 class CPropAccessor_Base
 {
 public:
@@ -199,15 +199,18 @@ public:
 	
 	T *GetPtr() const
 	{
+		uintptr_t base = (uintptr_t)this - *ADJUST;
+		
 		int off = -1;
 		assert(PROP->GetOffset(off));
-		return reinterpret_cast<T *>((uintptr_t)this + off);
+//		DevMsg("CPropAccessor_Base::GetPtr: base %08x off %08x size %08x dword %08x\n", base, off, sizeof(T), *(uint32_t *)(base + off));
+		return reinterpret_cast<T *>(base + off);
 	}
 };
 
 
-template<typename T, typename IPROP, IPROP *PROP>
-class CPropAccessor_Read : public CPropAccessor_Base<T, IPROP, PROP>
+template<typename T, typename IPROP, IPROP *PROP, size_t *ADJUST>
+class CPropAccessor_Read : public CPropAccessor_Base<T, IPROP, PROP, ADJUST>
 {
 public:
 	CPropAccessor_Read() = delete;
@@ -216,8 +219,8 @@ public:
 	const T& operator=(const T& val) = delete;
 };
 /* specialization for CHandle<U> */
-template<typename U, typename IPROP, IPROP *PROP>
-class CPropAccessor_Read<CHandle<U>, IPROP, PROP> : public CPropAccessor_Base<CHandle<U>, IPROP, PROP>
+template<typename U, typename IPROP, IPROP *PROP, size_t *ADJUST>
+class CPropAccessor_Read<CHandle<U>, IPROP, PROP, ADJUST> : public CPropAccessor_Base<CHandle<U>, IPROP, PROP, ADJUST>
 {
 public:
 	CPropAccessor_Read() = delete;
@@ -227,8 +230,8 @@ public:
 };
 
 
-template<typename T, typename IPROP, IPROP *PROP, bool NET = false>
-class CPropAccessor_Write : public CPropAccessor_Read<T, IPROP, PROP>
+template<typename T, typename IPROP, IPROP *PROP, size_t *ADJUST, bool NET = false>
+class CPropAccessor_Write : public CPropAccessor_Read<T, IPROP, PROP, ADJUST>
 {
 public:
 	CPropAccessor_Write() = delete;
@@ -244,8 +247,8 @@ public:
 	}
 };
 /* specialization for CHandle<U> */
-template<typename U, typename IPROP, IPROP *PROP, bool NET>
-class CPropAccessor_Write<CHandle<U>, IPROP, PROP, NET> : public CPropAccessor_Read<CHandle<U>, IPROP, PROP>
+template<typename U, typename IPROP, IPROP *PROP, size_t *ADJUST, bool NET>
+class CPropAccessor_Write<CHandle<U>, IPROP, PROP, ADJUST, NET> : public CPropAccessor_Read<CHandle<U>, IPROP, PROP, ADJUST>
 {
 public:
 	CPropAccessor_Write() = delete;
@@ -265,31 +268,37 @@ public:
 #define DEF_SENDPROP(T, P) \
 	typedef CProp_SendProp<T> _type_prop_##P; \
 	static _type_prop_##P s_prop_##P; \
-	/*typedef CPropAccessor_Write<T, _type_prop_##P, &s_prop_##P, true> _type_accessor_##P;*/ \
-	typedef CPropAccessor_Read<T, _type_prop_##P, &s_prop_##P> _type_accessor_##P; \
+	static size_t _adj_##P; \
+	/*typedef CPropAccessor_Write<T, _type_prop_##P, &s_prop_##P, _adj_##P, true> _type_accessor_##P;*/ \
+	typedef CPropAccessor_Read<T, _type_prop_##P, &s_prop_##P, &_adj_##P> _type_accessor_##P; \
 	_type_accessor_##P P; \
 	static_assert(std::is_empty<_type_accessor_##P>::value, "Prop accessor isn't an empty type")
 
 #define DEF_DATAMAP(T, P) \
 	typedef CProp_DataMap<T> _type_prop_##P; \
 	static _type_prop_##P s_prop_##P; \
-	typedef CPropAccessor_Write<T, _type_prop_##P, &s_prop_##P, false> _type_accessor_##P; \
+	static size_t _adj_##P; \
+	typedef CPropAccessor_Write<T, _type_prop_##P, &s_prop_##P, &_adj_##P, false> _type_accessor_##P; \
 	_type_accessor_##P P; \
 	static_assert(std::is_empty<_type_accessor_##P>::value, "Prop accessor isn't an empty type")
 
 #define DEF_EXTRACT(T, P) \
 	typedef CProp_Extract<T> _type_prop_##P; \
 	static _type_prop_##P s_prop_##P; \
-	typedef CPropAccessor_Write<T, _type_prop_##P, &s_prop_##P, false> _type_accessor_##P; \
+	static size_t _adj_##P; \
+	typedef CPropAccessor_Write<T, _type_prop_##P, &s_prop_##P, &_adj_##P, false> _type_accessor_##P; \
 	_type_accessor_##P P; \
 	static_assert(std::is_empty<_type_accessor_##P>::value, "Prop accessor isn't an empty type")
 
 
 #define IMPL_SENDPROP(T, C, P, SC) \
+	size_t C::_adj_##P = offsetof(C, P); \
 	CProp_SendProp<T> C::s_prop_##P(#C, #P, #SC)
 #define IMPL_DATAMAP(T, C, P) \
+	size_t C::_adj_##P = offsetof(C, P); \
 	CProp_DataMap<T> C::s_prop_##P(#C, #P)
 #define IMPL_EXTRACT(T, C, P, X) \
+	size_t C::_adj_##P = offsetof(C, P); \
 	CProp_Extract<T> C::s_prop_##P(#C, #P, X)
 
 
