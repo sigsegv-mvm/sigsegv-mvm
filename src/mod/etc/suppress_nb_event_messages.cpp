47e92ccaf0334143e64189bf153cd8619c3b070c
sigsegv <sigsegv@sigpipe.info>
2017-03-26 23:07:30 -0700

    Add simple/efficient mod to squash NB RESULT_CRITICAL collision msgs

---
+++ src/mod/etc/suppress_nb_event_messages.cpp
@@ -0,0 +1,46 @@
+#include "mod.h"
+#include "mem/scan.h"
+
+
+namespace Mod_Etc_Suppress_NB_Event_Messages
+{
+	const char *p_fmtstr = nullptr;
+	
+	
+	DETOUR_DECL_STATIC_CALL_CONVENTION(__gcc_regcall, SpewRetval_t, D__SpewMessage, SpewType_t spewType, const char *pGroupName, int nLevel, const Color *pColor, const char *pMsgFormat, va_list args)
+	{
+		/* intentional pointer comparison */
+		if (pMsgFormat == p_fmtstr) {
+			return SPEW_CONTINUE;
+		}
+		
+		return DETOUR_STATIC_CALL(D__SpewMessage)(spewType, pGroupName, nLevel, pColor, pMsgFormat, args);
+	}
+	
+	
+	class CMod : public IMod
+	{
+	public:
+		CMod() : IMod("Etc:Suppress_NB_Event_Messages")
+		{
+			MOD_ADD_DETOUR_STATIC(D__SpewMessage, "_SpewMessage [internal]");
+		}
+		
+		virtual bool OnLoad() override
+		{
+			p_fmtstr = Scan::FindUniqueConstStr(Library::SERVER, "%3.2f: WARNING: %s::%s() RESULT_CRITICAL collision\n");
+			if (p_fmtstr == nullptr) return false;
+			
+			return true;
+		}
+	};
+	CMod s_Mod;
+	
+	
+	ConVar cvar_enable("sig_etc_suppress_nb_event_messages", "0", FCVAR_NOTIFY,
+		"Mod: suppress NextBot event RESULT_CRITICAL collision console messages",
+		[](IConVar *pConVar, const char *pOldValue, float flOldValue) {
+			ConVarRef var(pConVar);
+			s_Mod.Toggle(var.GetBool());
+		});
+}
