0209c584dce4da4df729f9c8d24066ae208161d1
sigsegv <sigsegv@sigpipe.info>
2017-05-07 22:04:13 -0700

    Rework recipient filter stub into a much better implementation

--- src/stub/usermessages_sv.h
+++ src/stub/usermessages_sv.h
@@ -4,43 +4,201 @@
 
 #include "link/link.h"
 #include "stub/baseplayer.h"
+#include "util/iterate.h"
 
 
-class CReliableBroadcastRecipientFilter : public IRecipientFilter
+class CRecipientFilter : public IRecipientFilter
 {
 public:
-	CReliableBroadcastRecipientFilter()
-	{
-		this->m_Recipients.clear();
-		
-		for (int i = 1; i <= gpGlobals->maxClients; ++i) {
-			CBasePlayer *pPlayer = UTIL_PlayerByIndex(i);
-			if (pPlayer == nullptr) continue;
-			
-			this->m_Recipients.push_back(ENTINDEX(pPlayer));
-		}
-	}
-	virtual ~CReliableBroadcastRecipientFilter() {}
+	CRecipientFilter(bool reliable = false, bool init_msg = false) :
+		m_bReliable(reliable), m_bInitMessage(init_msg) {}
+	virtual ~CRecipientFilter() {};
 	
-	virtual bool IsReliable() const override { return true; }
-	virtual bool IsInitMessage() const override { return false; }
+	virtual bool IsReliable() const override    { return this->m_bReliable;    }
+	virtual bool IsInitMessage() const override { return this->m_bInitMessage; }
 	
-	virtual int GetRecipientCount() const override
-	{
-		return this->m_Recipients.size();
-	}
+	virtual int GetRecipientCount() const override { return this->m_Recipients.size(); }
 	virtual int GetRecipientIndex(int slot) const override
 	{
-		if (slot < 0 || slot >= (int)this->m_Recipients.size()) {
+		if (slot >= 0 && slot < (int)this->m_Recipients.size()) {
+			return this->m_Recipients[slot];
+		} else {
 			return -1;
 		}
-		
-		return this->m_Recipients[slot];
 	}
 	
+	void Reset()
+	{
+		this->m_bReliable    = false;
+		this->m_bInitMessage = false;
+		this->m_Recipients.clear();
+	}
+	
+	void MakeReliable()    { this->m_bReliable    = true; }
+	void MakeInitMessage() { this->m_bInitMessage = true; }
+	
+	void AddRecipient   (const CBasePlayer *player);
+	void RemoveRecipient(const CBasePlayer *player);
+	
+	void AddAllPlayers();
+	void RemoveAllRecipients();
+	
+	void AddRecipientsByPVS   (const Vector& origin);
+	void RemoveRecipientsByPVS(const Vector& origin);
+	
+	void AddRecipientsByPAS   (const Vector& origin);
+	void RemoveRecipientsByPAS(const Vector& origin);
+	
+//	void AddRecipientsByTeam   (CTeam *team); // TODO
+//	void RemoveRecipientsByTeam(CTeam *team); // TODO
+	
+	void AddPlayersFromBitMask   (const CBitVec<ABSOLUTE_PLAYER_LIMIT>& bits);
+	void RemovePlayersFromBitMask(const CBitVec<ABSOLUTE_PLAYER_LIMIT>& bits);
+	
 private:
+	bool m_bReliable;
+	bool m_bInitMessage;
 	std::vector<int> m_Recipients;
 };
+
+inline void CRecipientFilter::AddRecipient(const CBasePlayer *player)
+{
+	if (player == nullptr) return;
+	
+	int idx = ENTINDEX(player);
+	if (std::find(this->m_Recipients.begin(), this->m_Recipients.end(), idx) != this->m_Recipients.end()) return;
+	
+	this->m_Recipients.push_back(idx);
+}
+inline void CRecipientFilter::RemoveRecipient(const CBasePlayer *player)
+{
+	if (player == nullptr) return;
+	
+	auto it = std::find(this->m_Recipients.begin(), this->m_Recipients.end(), ENTINDEX(player));
+	if (it == this->m_Recipients.end()) return;
+	
+	this->m_Recipients.erase(it);
+}
+
+inline void CRecipientFilter::AddAllPlayers()
+{
+	this->m_Recipients.clear();
+	
+	ForEachPlayer([=](CBasePlayer *player){
+		this->AddRecipient(player);
+	});
+}
+inline void CRecipientFilter::RemoveAllRecipients()
+{
+	this->m_Recipients.clear();
+}
+
+inline void CRecipientFilter::AddRecipientsByPVS(const Vector& origin)
+{
+	CBitVec<ABSOLUTE_PLAYER_LIMIT> bits;
+	engine->Message_DetermineMulticastRecipients(false, origin, bits);
+	
+	this->AddPlayersFromBitMask(bits);
+	
+}
+inline void CRecipientFilter::RemoveRecipientsByPVS(const Vector& origin)
+{
+	CBitVec<ABSOLUTE_PLAYER_LIMIT> bits;
+	engine->Message_DetermineMulticastRecipients(false, origin, bits);
+	
+	this->RemovePlayersFromBitMask(bits);
+}
+
+inline void CRecipientFilter::AddRecipientsByPAS(const Vector& origin)
+{
+	CBitVec<ABSOLUTE_PLAYER_LIMIT> bits;
+	engine->Message_DetermineMulticastRecipients(true, origin, bits);
+	
+	this->AddPlayersFromBitMask(bits);
+}
+inline void CRecipientFilter::RemoveRecipientsByPAS(const Vector& origin)
+{
+	CBitVec<ABSOLUTE_PLAYER_LIMIT> bits;
+	engine->Message_DetermineMulticastRecipients(true, origin, bits);
+	
+	this->RemovePlayersFromBitMask(bits);
+}
+
+//inline void CRecipientFilter::AddRecipientsByTeam(CTeam *team)
+//{
+//	// TODO
+//}
+//inline void CRecipientFilter::RemoveRecipientsByTeam(CTeam *team)
+//{
+//	// TODO
+//}
+
+inline void CRecipientFilter::AddPlayersFromBitMask(const CBitVec<ABSOLUTE_PLAYER_LIMIT>& bits)
+{
+	for (int i = bits.FindNextSetBit(0); i != -1; i = bits.FindNextSetBit(i + 1)) {
+		CBasePlayer *player = UTIL_PlayerByIndex(i + 1);
+		if (player != nullptr) this->AddRecipient(player);
+	}
+}
+inline void CRecipientFilter::RemovePlayersFromBitMask(const CBitVec<ABSOLUTE_PLAYER_LIMIT>& bits)
+{
+	for (int i = bits.FindNextSetBit(0); i != -1; i = bits.FindNextSetBit(i + 1)) {
+		CBasePlayer *player = UTIL_PlayerByIndex(i + 1);
+		if (player != nullptr) this->RemoveRecipient(player);
+	}
+}
+
+
+template<bool RELIABLE>
+class CSingleUserRecipientFilterT : public CRecipientFilter
+{
+public:
+	CSingleUserRecipientFilterT(const CBasePlayer *player) : CRecipientFilter(RELIABLE)
+	{
+		this->AddRecipient(player);
+	}
+};
+
+template<bool RELIABLE>
+class CBroadcastRecipientFilterT : public CRecipientFilter
+{
+public:
+	CBroadcastRecipientFilterT() : CRecipientFilter(RELIABLE)
+	{
+		this->AddAllPlayers();
+	}
+};
+
+template<bool RELIABLE>
+class CPVSFilterT : public CRecipientFilter
+{
+public:
+	CPVSFilterT(const Vector& origin) : CRecipientFilter(RELIABLE)
+	{
+		this->AddRecipientsByPVS(origin);
+	}
+};
+
+template<bool RELIABLE>
+class CPASFilterT : public CRecipientFilter
+{
+public:
+	CPASFilterT(const Vector& origin) : CRecipientFilter(RELIABLE)
+	{
+		this->AddRecipientsByPAS(origin);
+	}
+};
+
+
+using CSingleUserRecipientFilter = CSingleUserRecipientFilterT<false>;
+using CBroadcastRecipientFilter  = CBroadcastRecipientFilterT <false>;
+using CPVSFilter                 = CPVSFilterT                <false>;
+using CPASFilter                 = CPASFilterT                <false>;
+
+using CReliableSingleUserRecipientFilter = CSingleUserRecipientFilterT<true>;
+using CReliableBroadcastRecipientFilter  = CBroadcastRecipientFilterT <true>;
+using CReliablePVSFilter                 = CPVSFilterT                <true>;
+using CReliablePASFilter                 = CPASFilterT                <true>;
 
 
 class CUserMessages_Server
