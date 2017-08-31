57e96f8f11fd594c5fb6c4f1188dc271925b8c7b
sigsegv <sigsegv@sigpipe.info>
2017-08-30 22:18:32 -0700

    Make IHotplugAction::UnloadAll less crashy by resetting intent iface

--- src/re/nextbot.cpp
+++ src/re/nextbot.cpp
@@ -1,17 +1,23 @@
 #include "re/nextbot.h"
+#include "stub/tfbot.h"
 
 
 void IHotplugAction::UnloadAll()
 {
-	while (!AutoList<IHotplugAction>::List().empty()) {
-		auto action = AutoList<IHotplugAction>::List().front();
+	std::set<CTFBot *> bots;
+	
+	for (auto action : AutoList<IHotplugAction>::List()) {
+		auto actor = action->GetActor();
+		if (actor == nullptr) continue;
 		
-		auto behavior = action->GetBehavior();
-		auto main_action = static_cast<Action<CTFBot> *>(behavior->FirstContainedResponder());
-		if (main_action != nullptr) {
-			main_action->ApplyResult(action->GetActor(), behavior, ActionResult<CTFBot>(ActionTransition::DONE, nullptr, "Hotplug action unloaded!"));
-			delete main_action;
-			behavior->m_MainAction = nullptr;
-		}
+		bots.insert(actor);
 	}
+	
+	Msg("IHotplugAction::UnloadAll: calling Reset() on %zu TFBot intention interfaces\n", bots.size());
+	
+	for (auto bot : bots) {
+		rtti_cast<INextBot *>(bot)->GetIntentionInterface()->Reset();
+	}
+	
+	assert(AutoList<IHotplugAction>::List().empty());
 }
