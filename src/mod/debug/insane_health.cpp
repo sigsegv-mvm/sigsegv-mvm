6369950ba93419ce968b0dc1589d41d89d0aa071
sigsegv <sigsegv@sigpipe.info>
2017-10-23 00:10:31 -0700

    Add mod for testing ridiculous health and maxhealth values

---
+++ src/mod/debug/insane_health.cpp
@@ -0,0 +1,25 @@
+#include "mod.h"
+#include "stub/tfplayer.h"
+#include "util/iterate.h"
+
+
+namespace Mod_Debug_Insane_Health
+{
+	CON_COMMAND_F(sig_debug_insane_health, "Debug: set HP to given value and see what happens", FCVAR_CHEAT)
+	{
+		if (args.ArgC() < 2) return;
+		
+		int64_t hp_target = strtoll(args[1], nullptr, 0);
+		
+		ForEachTFPlayer([=](CTFPlayer *player){
+			if (!player->IsAlive()) return;
+			
+			float maxhp_to_add = (hp_target - player->GetMaxHealth());
+			player->AddCustomAttribute("hidden maxhealth non buffed", maxhp_to_add, 3600.0f);
+			
+			player->SetHealth(hp_target);
+			
+			Msg("Set health of '%s' to %" PRIi64 "\n", player->GetPlayerName(), hp_target);
+		});
+	}
+}
