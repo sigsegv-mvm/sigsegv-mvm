09fe9d04ae4b0b400780341daf501c24892a3c1c
sigsegv <sigsegv@sigpipe.info>
2018-03-08 00:03:20 -0800

    Fix some issues related to calling vfuncs in ctor/dtor contexts

--- src/stub/igamesystem.cpp
+++ src/stub/igamesystem.cpp
@@ -9,12 +9,14 @@ static bool IGameSystem_IsRegistered(IGameSystem *pSys)
 	return (std::find(s_RegisteredSystems.begin(), s_RegisteredSystems.end(), pSys) != s_RegisteredSystems.end());
 }
 
+#if 0
 static bool IGameSystem_IsRegistered(const char *name)
 {
 	return (std::find_if(s_RegisteredSystems.begin(), s_RegisteredSystems.end(), [=](IGameSystem *pSys){
 		return (pSys->Name() != nullptr && strcmp(pSys->Name(), name) == 0);
 	}) != s_RegisteredSystems.end());
 }
+#endif
 
 
 static StaticFuncThunk<void, IGameSystem *> ft_IGameSystem_Add("IGameSystem::Add");
@@ -32,14 +34,14 @@ static void IGameSystem_SafeAdd(IGameSystem *pSys)
 	if (!IGameSystem_AreFuncsLinked()) return;
 	
 	if (IGameSystem_IsRegistered(pSys)) {
-		Msg("[IGameSystem::SafeAdd] Game system \"%s\" is already registered!\n", pSys->Name());
+//		Msg("[IGameSystem::SafeAdd] Game system \"%s\" is already registered!\n", pSys->Name());
 		return;
 	}
 	
 	IGameSystem_ActualAdd(pSys);
 	s_RegisteredSystems.push_back(pSys);
 	
-	Msg("[IGameSystem::SafeAdd] Game system \"%s\" registered successfully.\n", pSys->Name());
+//	Msg("[IGameSystem::SafeAdd] Game system \"%s\" registered successfully.\n", pSys->Name());
 }
 
 static void IGameSystem_SafeRemove(IGameSystem *pSys)
@@ -47,14 +49,14 @@ static void IGameSystem_SafeRemove(IGameSystem *pSys)
 	if (!IGameSystem_AreFuncsLinked()) return;
 	
 	if (!IGameSystem_IsRegistered(pSys)) {
-		Msg("[IGameSystem::SafeRemove] Game system \"%s\" is not registered!\n", pSys->Name());
+//		Msg("[IGameSystem::SafeRemove] Game system \"%s\" is not registered!\n", pSys->Name());
 		return;
 	}
 	
 	IGameSystem_ActualRemove(pSys);
 	std::remove(s_RegisteredSystems.begin(), s_RegisteredSystems.end(), pSys);
 	
-	Msg("[IGameSystem::SafeRemove] Game system \"%s\" unregistered successfully.\n", pSys->Name());
+//	Msg("[IGameSystem::SafeRemove] Game system \"%s\" unregistered successfully.\n", pSys->Name());
 }
 
 
