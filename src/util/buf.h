ab25fcae8e5070e0cdc703344a13aaac7215cef8
sigsegv <sigsegv@sigpipe.info>
2018-04-23 19:38:02 -0700

    Modernize/improve ByteBuf class

--- src/util/buf.h
+++ src/util/buf.h
@@ -5,37 +5,37 @@
 class ByteBuf
 {
 public:
-	ByteBuf(int size);
-	virtual ~ByteBuf();
+	ByteBuf(size_t size);
+	~ByteBuf();
 	
-	uint8_t& operator[](int idx);
-	const uint8_t& operator[](int idx) const;
+	uint8_t& operator[](size_t idx);
+	const uint8_t& operator[](size_t idx) const;
 	
 	void SetAll(uint8_t val);
-	void SetRange(int idx, int len, uint8_t val);
+	void SetRange(size_t idx, size_t len, uint8_t val);
 	
-	uint32_t GetDword(int idx);
-	void SetDword(int idx, uint32_t val);
+	uint32_t GetDword(size_t idx) const;
+	void SetDword(size_t idx, uint32_t val);
 	
-	float GetFloat(int idx);
-	void SetFloat(int idx, float val);
+	float GetFloat(size_t idx) const;
+	void SetFloat(size_t idx, float val);
 	
 	void CopyFrom(const ByteBuf& that);
 	void CopyFrom(const uint8_t *arr);
 	
 	void Dump() const;
 	
-	int GetSize() const { return this->m_iSize; }
+	size_t GetSize() const { return this->m_iSize; }
 	
 	const uint8_t *GetBufPtr() const { return this->m_Buf; }
 	
 private:
-	const int m_iSize;
+	const size_t m_iSize;
 	uint8_t *m_Buf = nullptr;
 };
 
 
-inline ByteBuf::ByteBuf(int size) :
+inline ByteBuf::ByteBuf(size_t size) :
 	m_iSize(size)
 {
 	this->m_Buf = new uint8_t[size];
@@ -48,13 +48,13 @@ inline ByteBuf::~ByteBuf()
 }
 
 
-inline uint8_t& ByteBuf::operator[](int idx)
+inline uint8_t& ByteBuf::operator[](size_t idx)
 {
 	assert(idx >= 0 && idx < this->m_iSize);
 	return this->m_Buf[idx];
 }
 
-inline const uint8_t& ByteBuf::operator[](int idx) const
+inline const uint8_t& ByteBuf::operator[](size_t idx) const
 {
 	assert(idx >= 0 && idx < this->m_iSize);
 	return this->m_Buf[idx];
@@ -63,63 +63,60 @@ inline const uint8_t& ByteBuf::operator[](int idx) const
 
 inline void ByteBuf::SetAll(uint8_t val)
 {
-	memset(this->m_Buf, val, this->m_iSize);
+	std::fill_n(this->m_Buf, this->m_iSize, val);
 }
 
-inline void ByteBuf::SetRange(int idx, int len, uint8_t val)
+inline void ByteBuf::SetRange(size_t idx, size_t len, uint8_t val)
 {
 	assert(idx >= 0 && idx + len <= this->m_iSize);
-	
-	for (int i = 0; i < len; ++i) {
-		this->m_Buf[idx + i] = val;
-	}
+	std::fill_n(this->m_Buf + idx, len, val);
 }
 
 
-inline uint32_t ByteBuf::GetDword(int idx)
+inline uint32_t ByteBuf::GetDword(size_t idx) const
 {
-	assert(idx >= 0 && idx + 4 <= this->m_iSize);
-	return *(uint32_t *)(this->m_Buf + idx);
+	assert(idx >= 0 && idx + sizeof(uint32_t) <= this->m_iSize);
+	return *reinterpret_cast<uint32_t *>(this->m_Buf + idx);
 }
 
-inline void ByteBuf::SetDword(int idx, uint32_t val)
+inline void ByteBuf::SetDword(size_t idx, uint32_t val)
 {
-	assert(idx >= 0 && idx + 4 <= this->m_iSize);
-	*(uint32_t *)(this->m_Buf + idx) = val;
+	assert(idx >= 0 && idx + sizeof(uint32_t) <= this->m_iSize);
+	*reinterpret_cast<uint32_t *>(this->m_Buf + idx) = val;
 }
 
 
-inline float ByteBuf::GetFloat(int idx)
+inline float ByteBuf::GetFloat(size_t idx) const
 {
-	assert(idx >= 0 && idx + 4 <= this->m_iSize);
-	return *(float *)(this->m_Buf + idx);
+	assert(idx >= 0 && idx + sizeof(float) <= this->m_iSize);
+	return *reinterpret_cast<float *>(this->m_Buf + idx);
 }
 
-inline void ByteBuf::SetFloat(int idx, float val)
+inline void ByteBuf::SetFloat(size_t idx, float val)
 {
-	assert(idx >= 0 && idx + 4 <= this->m_iSize);
-	*(float *)(this->m_Buf + idx) = val;
+	assert(idx >= 0 && idx + sizeof(float) <= this->m_iSize);
+	*reinterpret_cast<float *>(this->m_Buf + idx) = val;
 }
 
 
 inline void ByteBuf::CopyFrom(const ByteBuf& that)
 {
 	assert(this->m_iSize == that.m_iSize);
-	memcpy(this->m_Buf, that.m_Buf, this->m_iSize);
+	std::copy_n(that.m_Buf, this->m_iSize, this->m_Buf);
 }
 
 inline void ByteBuf::CopyFrom(const uint8_t *arr)
 {
-	memcpy(this->m_Buf, arr, this->m_iSize);
+	std::copy_n(arr, this->m_iSize, this->m_Buf);
 }
 
 
 inline void ByteBuf::Dump() const
 {
-	constexpr int bytes_per_line  = 16;
-	constexpr int bytes_per_group =  4;
+	constexpr size_t bytes_per_line  = 16;
+	constexpr size_t bytes_per_group =  4;
 	
-	int addr_digits = [](int size){
+	int addr_digits = [](size_t size){
 		if (size <= (1 <<  8)) return 2;
 		if (size <= (1 << 16)) return 4;
 		if (size <= (1 << 24)) return 6;
@@ -127,10 +124,10 @@ inline void ByteBuf::Dump() const
 	}(this->GetSize());
 	
 	DevMsg("   %*s__00_01_02_03__04_05_06_07__08_09_0A_0B__0C_0D_0E_0F__\n", addr_digits, "");
-	for (int i = 0; i < this->GetSize(); i += bytes_per_line) {
+	for (size_t i = 0; i < this->GetSize(); i += bytes_per_line) {
 		std::string line = CFmtStrN<16>("+0x%0*X", addr_digits, i).Get();
 		
-		for (int j = i; j < this->GetSize() && j < i + bytes_per_line; ++j) {
+		for (size_t j = i; j < this->GetSize() && j < i + bytes_per_line; ++j) {
 			line += CFmtStrN<16>("%*s%02X", ((j % bytes_per_group == 0) ? 2 : 1), "", this->m_Buf[j]).Get();
 		}
 		
